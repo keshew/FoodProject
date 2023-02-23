d1f5b92096cbde4d890de745a4ec3a87f3f3382e
keshew <artyom.korotckov2017@yandex.ru>
2023-02-23 20:58:39 +0500

    Initial Commit

---
+++ FoodProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodProject
+//
+//  Created by Артём Коротков on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
